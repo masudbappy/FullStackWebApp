ffafb5b995991cb60aef671c08a63638c48e0e1f
masudbappy <masud.bappy90@gmail.com>
2019-09-07 01:38:17 +0600

    registration complete

--- src/main/java/com/masudbappy/fullstackwebapp/services/UserService.java
+++ src/main/java/com/masudbappy/fullstackwebapp/services/UserService.java
@@ -16,7 +16,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(User user){
+    public void createUser(User user) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
         Role userRole = new Role("USER");
@@ -25,7 +25,8 @@ public class UserService {
         user.setRoles(roles);
         userRepository.save(user);
     }
-    public void createAdmin(User user){
+
+    public void createAdmin(User user) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
         Role userRole = new Role("ADMIN");
@@ -35,7 +36,15 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public User findOne(String email){
+    public User findOne(String email) {
         return userRepository.getOne(email);
+    }
+
+    public boolean isUserPresent(String email) {
+        User u = userRepository.getOne(email);
+        if (u != null) {
+            return true;
+        }
+        return false;
     }
 }
