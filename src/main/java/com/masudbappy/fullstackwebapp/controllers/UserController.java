4b2e7f922aabfee0bd1bbe7d9d65c62e58d72f6e
masudbappy <masud.bappy90@gmail.com>
2019-09-07 23:03:31 +0600

    usercontroller has been updated

--- src/main/java/com/masudbappy/fullstackwebapp/controllers/UserController.java
+++ src/main/java/com/masudbappy/fullstackwebapp/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -12,8 +13,8 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/users")
-    public String listUsers(Model model){
-        model.addAttribute("users",userService.findAll());
+    public String listUsers(Model model, @RequestParam(defaultValue = "") String name){
+        model.addAttribute("users",userService.findByName(name));
         return "views/list";
     }
 }
