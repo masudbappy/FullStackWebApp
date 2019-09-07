3d8d8e11f721fd0067f5cfee74eb7e1c6cce3936
masudbappy <masud.bappy90@gmail.com>
2019-09-07 23:32:13 +0600

    src/main/java/com/masudbappy/fullstackwebapp/controllers/TaskController.java

--- src/main/java/com/masudbappy/fullstackwebapp/controllers/TaskController.java
+++ src/main/java/com/masudbappy/fullstackwebapp/controllers/TaskController.java
@@ -2,12 +2,16 @@ package com.masudbappy.fullstackwebapp.controllers;
 
 import com.masudbappy.fullstackwebapp.entities.Task;
 import com.masudbappy.fullstackwebapp.services.TaskService;
+import com.masudbappy.fullstackwebapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 public class TaskController {
@@ -15,10 +19,22 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/addTask")
     public String taskForm(String email, Model model, HttpSession session){
         session.setAttribute("email", email);
         model.addAttribute("task", new Task());
         return "views/taskForm";
+    }
+    @PostMapping("/addTask")
+    public String addTask(@Valid Task task, BindingResult bindingResult, HttpSession session){
+        if (bindingResult.hasErrors()){
+            return "views/taskForm";
+        }
+        String email = (String) session.getAttribute("email");
+        taskService.addTask(task, userService.findOne(email));
+        return "redirect:/users";
     }
 }
